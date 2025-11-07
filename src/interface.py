65151662dde8965b097c9c1543487715f090c313
Mahyar Jahani Nasab <jahaninasab.mahyar@gmail.com>
2025-11-07 16:09:56 +1100

    Interface for the first layer

--- src/interface.py
+++ src/interface.py
@@ -13,4 +13,42 @@ class StreamRecComponent(ABC):
         #have it
         #get it from the parent class
         pass
+
+
+class DataCollector(StreamRecComponent):
+    def __init__(self):
+        super().__init__()
+        self.name = "HuggingFaceDataCollector"
+        self.description = "Collect dataset from ready to collect databses in Hugging Face"
     
+    @abstractmethod
+    def request(self):
+        pass
+
+    @abstractmethod
+    def to_paraquet(self):
+        pass 
+
+
+class DataCleaner(StreamRecComponent):
+    def __init__(self):
+        super().__init__()
+        self.name = "Handling data in parallel using PySpark"
+        self.description = "Cleaning Data and turning it into datalake"
+    
+    @abstractmethod
+    def to_datalake(self):
+        pass
+
+    @abstractmethod
+    def transform(self):
+        pass
+
+    @abstractmethod
+    def _filter(self):
+        pass 
+
+    @abstractmethod
+    def _feature_creation(self):
+        pass
+
