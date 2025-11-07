616f0ce26206ba6526f531e96390ea8b3a50a1be
Mahyar Jahani Nasab <jahaninasab.mahyar@gmail.com>
2025-11-07 15:19:40 +1100

    writing interface for the source code

---
+++ src/interface.py
@@ -0,0 +1,16 @@
+from abc import ABC, abstractmethod
+
+class StreamRecComponent(ABC):
+
+    def __init__(self):
+        self.name = ""
+        self.config = {}
+        self.description = ""
+    
+    def get_meta(self):
+
+        #1 get it from internet
+        #have it
+        #get it from the parent class
+        pass
+    
